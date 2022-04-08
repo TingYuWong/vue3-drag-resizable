a2c6042e22b3bb2af473a7a8841128b801002c6d
翁婷瑜 <t13736@ms.sltung.com.tw>
2022-04-08 13:48:42 +0800

    初步使用resize observer 取得 div resize的寬高

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <DragResize />
+</template>
+
+<script>
+import DragResize from './components/DragResize.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DragResize
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DragResize.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="wrap" ref="wrap">Hey! I am gonna become draggable and resizable.</div>
+</template>
+
+<script>
+export default {
+    name: 'DragResize',
+    mounted() {
+        this.observeEle()
+    },
+    methods: {
+        observeEle()  {
+            const resizeObj = new ResizeObserver(entries => {
+                let rect = entries[0].contentRect
+                console.log('element', entries[0].target)
+                console.log('width', rect.width)
+                console.log('height', rect.height)
+            })
+
+            resizeObj.observe(this.$refs.wrap)
+        },
+    },
+}
+</script>
+
+<style scoped>
+.wrap {
+    background-color: rgb(112, 96, 255);
+    color: #fff;
+    border-radius: 10px;
+    padding: 5px;
+    width: 200px;
+    cursor: pointer;
+    user-select: none;
+    resize: both;
+    overflow: auto;
+}
+</style>
